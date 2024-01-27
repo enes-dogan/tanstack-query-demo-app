0320c20ebcc335aa7e25432e1f6074d8c14b91cb
enes-dogan <enesdogan210@gmail.com>
2024-01-27 22:21:45 +0300

    header link added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Header({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div id="main-header-loading"></div>
       <header id="main-header">
         <div id="header-title">
-          <h1>React Events</h1>
+          <h1>
+            <Link to="/">React Events</Link>
+          </h1>
         </div>
         <nav>{children}</nav>
       </header>
